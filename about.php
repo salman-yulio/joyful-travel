2c7bcf7c78a879db1772a34b890305c8a6de45f0
salmanyulio <muhammadyulio30@smk.belajar.id>
2024-12-16 09:18:35 +0700

    Menambahkan link icon bootstrap pada halaman about dan contact

--- about.php
+++ about.php
@@ -21,7 +21,8 @@ check_roles(['visitor']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Joyful Travel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  </head>
 <body>
     
 <?php include('navbar.php'); ?>
